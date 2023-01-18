68759a3cac6fa7d0a3047d929caa7d92a3ee525b
Zack Adams <info@zadams.com>
2023-01-17 23:54:07 -0500

    acts_as_tenant required tenant bypass

--- app/jobs/mass_encryption/batch_encryption_job.rb
+++ app/jobs/mass_encryption/batch_encryption_job.rb
@@ -1,8 +1,17 @@
 class MassEncryption::BatchEncryptionJob < MassEncryption::ApplicationJob
   def perform(batch, auto_enqueue_next: true)
-    if batch.present?
-      batch.encrypt_now
-      self.class.perform_later batch.next if auto_enqueue_next
+      encrypt_batch = -> {
+        if batch.present?
+          batch.encrypt_now
+          self.class.perform_later batch.next if auto_enqueue_next
+        end
+      }
+    if defined?(ActsAsTenant)
+      ActsAsTenant.without_tenant do
+        encrypt_batch.call
+      end
+    else
+      encrypt_batch.call
     end
   end
 
